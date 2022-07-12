8bfc1f9ea91a2770b683fb480dc6ce3fbae56e99
Arseniy Khvorov <khvorov45@gmail.com>
2022-07-12 11:00:13 +1000

    remove script

--- copy.sh
+++
@@ -1,3 +0,0 @@
-rsync -rv /home/khvorova/Projects/vaccine-meeting/ /home/khvorova/vidrlwhoflu/Group/personnel/Arseniy/vaccine-meeting/ --exclude *.fasta
-
-rsync -rv /home/khvorova/Projects/vaccine-meeting/ /home/khvorova/ownCloud/vaccine-meeting/ --exclude *.fasta
