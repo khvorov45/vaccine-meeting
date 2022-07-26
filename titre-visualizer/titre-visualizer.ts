31692e2cc8a0673718163f825e839392e0ed3273
Arseniy Khvorov <khvorov45@gmail.com>
2022-07-26 13:14:53 +1000

    move functions

--- titre-visualizer/titre-visualizer.ts
+++ titre-visualizer/titre-visualizer.ts
@@ -283,6 +283,35 @@ const desiredOrderSort = (ord: any[]) => {
 	}
 }
 
+const colChannel255ToString = (channel: number) => {
+	return channel.toString(16).padStart(2, "0")
+}
+
+const colChangeSaturation = (col: string, satDelta: number) => {
+	let alpha = col.slice(7, 9)
+	let red = parseInt(col.slice(1, 3), 16)
+	let green = parseInt(col.slice(3, 5), 16)
+	let blue = parseInt(col.slice(5, 7), 16)
+
+	let mean = (red + green + blue) / 3
+
+	red = (red - mean) * satDelta + mean
+	green = (green - mean) * satDelta + mean
+	blue = (blue - mean) * satDelta + mean
+
+	red = Math.max(Math.min(Math.round(red), 255), 0)
+	green = Math.max(Math.min(Math.round(green), 255), 0)
+	blue = Math.max(Math.min(Math.round(blue), 255), 0)
+
+	let redNew = colChannel255ToString(red)
+	let greenNew = colChannel255ToString(green)
+	let blueNew = colChannel255ToString(blue)
+
+	return "#" + redNew + greenNew + blueNew + alpha
+}
+
+const isGood = (n: any) => n !== null && n !== undefined && !isNaN(n)
+
 //
 // SECTION DOM
 //
@@ -384,35 +413,6 @@ const createSwitch = <SingleOpt extends string | number, OptType extends SingleO
 
 	return switchElement
 }
-
-const colChannel255ToString = (channel: number) => {
-	return channel.toString(16).padStart(2, "0")
-}
-
-const colChangeSaturation = (col: string, satDelta: number) => {
-	let alpha = col.slice(7, 9)
-	let red = parseInt(col.slice(1, 3), 16)
-	let green = parseInt(col.slice(3, 5), 16)
-	let blue = parseInt(col.slice(5, 7), 16)
-
-	let mean = (red + green + blue) / 3
-
-	red = (red - mean) * satDelta + mean
-	green = (green - mean) * satDelta + mean
-	blue = (blue - mean) * satDelta + mean
-
-	red = Math.max(Math.min(Math.round(red), 255), 0)
-	green = Math.max(Math.min(Math.round(green), 255), 0)
-	blue = Math.max(Math.min(Math.round(blue), 255), 0)
-
-	let redNew = colChannel255ToString(red)
-	let greenNew = colChannel255ToString(green)
-	let blueNew = colChannel255ToString(blue)
-
-	return "#" + redNew + greenNew + blueNew + alpha
-}
-
-const isGood = (n: any) => n !== null && n !== undefined && !isNaN(n)
 
 //
 // SECTION Plots
