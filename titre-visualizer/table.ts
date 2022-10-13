1f10b201b6d22305b197db5a2e489316ccd47c03
Arseniy Khvorov <khvorov45@gmail.com>
2022-10-13 14:46:17 +1100

    inline virtualized list

--- titre-visualizer/table.ts
+++ titre-visualizer/table.ts
@@ -1,4 +1,348 @@
-import * as VL from "./virtualized-list.ts"
+// Stripped down version of https://github.com/patrick-steele-idem/morphdom
+const ELEMENT_NODE = 1
+const TEXT_NODE = 3
+const COMMENT_NODE = 8
+
+const walkDiscardedChildNodes = (node: Node, skipKeyedNodes: boolean) => {
+	if (node.nodeType === ELEMENT_NODE) {
+		let curChild = node.firstChild
+		while (curChild) {
+			if (curChild.firstChild) {
+				walkDiscardedChildNodes(curChild, skipKeyedNodes)
+			}
+			curChild = curChild.nextSibling
+		}
+	}
+}
+
+const removeNode = (node: Node, parentNode: Node, skipKeyedNodes: boolean) => {
+	if (parentNode) {
+		parentNode.removeChild(node)
+	}
+	walkDiscardedChildNodes(node, skipKeyedNodes)
+}
+
+const compareNodeNames = (fromEl: Node, toEl: Node) => {
+	const fromNodeName = fromEl.nodeName
+	const toNodeName = toEl.nodeName
+
+	if (fromNodeName === toNodeName) {
+		return true
+	}
+
+	const fromCodeStart = fromNodeName.charCodeAt(0)
+	const toCodeStart = toNodeName.charCodeAt(0)
+
+	// If the target element is a virtual DOM node or SVG node then we may
+	// need to normalize the tag name before comparing. Normal HTML elements that are
+	// in the "http://www.w3.org/1999/xhtml"
+	// are converted to upper case
+	if (fromCodeStart <= 90 && toCodeStart >= 97) {
+		// from is upper and to is lower
+		return fromNodeName === toNodeName.toUpperCase()
+	} else if (toCodeStart <= 90 && fromCodeStart >= 97) {
+		// to is upper and from is lower
+		return toNodeName === fromNodeName.toUpperCase()
+	} else {
+		return false
+	}
+}
+
+const morphEl = (fromEl: Node, toEl: Node) => {
+	let curToNodeChild = toEl.firstChild
+	let curFromNodeChild = fromEl.firstChild
+
+	let fromNextSibling = null
+	let toNextSibling
+
+	// walk the children
+	outer: while (curToNodeChild) {
+		toNextSibling = curToNodeChild.nextSibling
+
+		// walk the fromNode children all the way through
+		while (curFromNodeChild !== null) {
+			fromNextSibling = curFromNodeChild.nextSibling
+
+			if (curToNodeChild.isSameNode && curToNodeChild.isSameNode(curFromNodeChild)) {
+				curToNodeChild = toNextSibling
+				curFromNodeChild = fromNextSibling
+				continue outer
+			}
+
+			const curFromNodeType = curFromNodeChild.nodeType
+
+			// this means if the curFromNodeChild doesnt have a match with the curToNodeChild
+			let isCompatible = undefined
+
+			if (curFromNodeType === curToNodeChild.nodeType) {
+				if (curFromNodeType === ELEMENT_NODE && curFromNodeChild) {
+					isCompatible = isCompatible !== false && compareNodeNames(curFromNodeChild, curToNodeChild)
+					if (isCompatible) {
+						// We found compatible DOM elements so transform
+						// the current "from" node to match the current
+						// target DOM node.
+						// MORPH
+						morphEl(curFromNodeChild, curToNodeChild)
+					}
+				} else if (curFromNodeType === TEXT_NODE || curFromNodeType == COMMENT_NODE) {
+					// Both nodes being compared are Text or Comment nodes
+					isCompatible = true
+					// Simply update nodeValue on the original node to
+					// change the text value
+					if (curFromNodeChild.nodeValue !== curToNodeChild.nodeValue) {
+						curFromNodeChild.nodeValue = curToNodeChild.nodeValue
+					}
+				}
+			}
+
+			if (isCompatible) {
+				// Advance both the "to" child and the "from" child since we found a match
+				// Nothing else to do as we already recursively called morphChildren above
+				curToNodeChild = toNextSibling
+				curFromNodeChild = fromNextSibling
+				continue outer
+			}
+
+			if (curFromNodeChild !== null) {
+				removeNode(curFromNodeChild, fromEl, true /* skip keyed nodes */)
+			}
+
+			curFromNodeChild = fromNextSibling
+		} // while(curFromNodeChild)
+
+		// If we got this far then we did not find a candidate match for
+		// our "to node" and we exhausted all of the children "from"
+		// nodes. Therefore, we will just append the current "to" node
+		// to the end
+		fromEl.appendChild(curToNodeChild)
+
+		curToNodeChild = toNextSibling
+		curFromNodeChild = fromNextSibling
+	}
+
+	// We have processed all of the "to nodes". If curFromNodeChild is
+	// non-null then we still have some from nodes left over that need
+	// to be removed
+	while (curFromNodeChild) {
+		const fromNextSibling = curFromNodeChild.nextSibling
+		removeNode(curFromNodeChild, fromEl, true /* skip keyed nodes */)
+		curFromNodeChild = fromNextSibling
+	}
+}
+
+const morphdom = (fromNode: HTMLElement, toNode: HTMLElement) => {
+	if (fromNode !== toNode) {
+		if (!(toNode.isSameNode && toNode.isSameNode(fromNode))) {
+			morphEl(fromNode, toNode)
+		}
+	}
+}
+
+// Based on https://github.com/clauderic/virtualized-list
+
+type SizeAndPosition = {
+	itemCount: number
+	itemSize: number
+	// Cache of size and position data for items, mapped by item index.
+	itemSizeAndPositionData: { [key in number]: { offset: number; size: number } }
+	// Measurements for items up to this index can be trusted; items afterward should be estimated.
+	lastMeasuredIndex: number
+}
+
+const createSizeAndPosition = (itemCount: number, itemSize: number) => {
+	const result: SizeAndPosition = {
+		itemCount: itemCount,
+		itemSize: itemSize,
+		itemSizeAndPositionData: [],
+		lastMeasuredIndex: -1,
+	}
+	return result
+}
+
+const getSizeAndPositionOfLastMeasuredItem = (sp: SizeAndPosition) => {
+	return sp.lastMeasuredIndex >= 0 ? sp.itemSizeAndPositionData[sp.lastMeasuredIndex] : { offset: 0, size: 0 }
+}
+
+const getTotalSize = (sp: SizeAndPosition) => {
+	const lastMeasuredSizeAndPosition = getSizeAndPositionOfLastMeasuredItem(sp)
+	return (
+		lastMeasuredSizeAndPosition.offset +
+		lastMeasuredSizeAndPosition.size +
+		(sp.itemCount - sp.lastMeasuredIndex - 1) * sp.itemSize
+	)
+}
+
+const getSizeAndPositionForIndex = (sp: SizeAndPosition, index: number) => {
+	index = Math.max(Math.min(index, sp.itemCount - 1), 0)
+
+	if (index > sp.lastMeasuredIndex) {
+		const lastMeasuredSizeAndPosition = getSizeAndPositionOfLastMeasuredItem(sp)
+		let offset = lastMeasuredSizeAndPosition.offset + lastMeasuredSizeAndPosition.size
+
+		for (let i = sp.lastMeasuredIndex + 1; i <= index; i++) {
+			const size = sp.itemSize
+			sp.itemSizeAndPositionData[i] = {
+				offset,
+				size,
+			}
+			offset += size
+		}
+
+		sp.lastMeasuredIndex = index
+	}
+
+	return sp.itemSizeAndPositionData[index]
+}
+
+/**
+ * Searches for the item (index) nearest the specified offset.
+ *
+ * If no exact match is found the next lowest item index will be returned.
+ * This allows partially visible items (with offsets just before/above the fold) to be visible.
+ */
+const findNearestItem = (sp: SizeAndPosition, offset: number) => {
+	// Our search algorithms find the nearest match at or below the specified offset.
+	// So make sure the offset is at least 0 or no match will be found.
+	offset = Math.max(0, offset)
+
+	const lastMeasuredSizeAndPosition = getSizeAndPositionOfLastMeasuredItem(sp)
+	const lastMeasuredIndex = Math.max(0, sp.lastMeasuredIndex)
+
+	if (lastMeasuredSizeAndPosition.offset >= offset) {
+		// If we've already measured items within this range just use a binary search as it's faster.
+		return binarySearch(sp, 0, lastMeasuredIndex, offset)
+	} else {
+		// If we haven't yet measured this high, fallback to an exponential search with an inner binary search.
+		// The exponential search avoids pre-computing sizes for the full set of items as a binary search would.
+		// The overall complexity for this approach is O(log n).
+		return exponentialSearch(sp, lastMeasuredIndex, offset)
+	}
+}
+
+const getVisibleRange = (sp: SizeAndPosition, containerSize: number, offset: number) => {
+	const totalSize = getTotalSize(sp)
+
+	if (totalSize === 0) {
+		return { start: 0, stop: 0 }
+	}
+
+	const maxOffset = offset + containerSize
+	const start = findNearestItem(sp, offset)
+	let stop = start
+
+	const datum = getSizeAndPositionForIndex(sp, start)
+	offset = datum.offset + datum.size
+
+	while (offset < maxOffset && stop < sp.itemCount - 1) {
+		stop++
+		offset += getSizeAndPositionForIndex(sp, stop).size
+	}
+
+	return {
+		start,
+		stop,
+	}
+}
+
+const binarySearch = (sp: SizeAndPosition, low: number, high: number, offset: number) => {
+	let middle
+	let currentOffset
+
+	while (low <= high) {
+		middle = low + Math.floor((high - low) / 2)
+		currentOffset = getSizeAndPositionForIndex(sp, middle).offset
+
+		if (currentOffset === offset) {
+			return middle
+		} else if (currentOffset < offset) {
+			low = middle + 1
+		} else if (currentOffset > offset) {
+			high = middle - 1
+		}
+	}
+
+	if (low > 0) {
+		return low - 1
+	}
+
+	return low
+}
+
+const exponentialSearch = (sp: SizeAndPosition, index: number, offset: number) => {
+	let interval = 1
+	while (index < sp.itemCount && getSizeAndPositionForIndex(sp, index).offset < offset) {
+		index += interval
+		interval *= 2
+	}
+	return binarySearch(sp, Math.floor(index / 2), Math.min(index, sp.itemCount - 1), offset)
+}
+
+export type VirtualizedListOptions = {
+	height: number
+	rowCount: number
+	renderRow: (rowIndex: number) => HTMLElement
+	rowHeight: number
+}
+
+export type VirtualizedList = {
+	container: HTMLElement
+	inner: HTMLElement
+	content: HTMLElement
+	height: number
+	offset: number
+	sizeAndPosition: SizeAndPosition
+	renderRow: (rowIndex: number) => HTMLElement
+}
+
+export const createVirtualizedList = (container: HTMLElement, options: VirtualizedListOptions) => {
+	const vl: VirtualizedList = {
+		container: container,
+		height: options.height,
+		offset: 0,
+		sizeAndPosition: createSizeAndPosition(options.rowCount, options.rowHeight),
+		inner: document.createElement("div"),
+		content: document.createElement("div"),
+		renderRow: options.renderRow,
+	}
+
+	vl.inner.setAttribute(
+		"style",
+		"position:relative; overflow:hidden; width:100%; min-height:100%; will-change: transform;"
+	)
+	vl.content.setAttribute("style", "position:absolute; top:0; left:0; height:100%; width:100%; overflow:visible;")
+	vl.inner.appendChild(vl.content)
+	container.appendChild(vl.inner)
+
+	container.scrollTop = vl.offset
+	container.addEventListener("scroll", () => {
+		vl.offset = vl.container.scrollTop
+		requestAnimationFrame(() => vlRender(vl))
+	})
+
+	requestAnimationFrame(() => vlRender(vl))
+	return vl
+}
+
+export const setHeight = (vl: VirtualizedList, height: number) => {
+	vl.height = height
+	requestAnimationFrame(() => vlRender(vl))
+}
+
+export const setRowCount = (vl: VirtualizedList, count: number) => {
+	vl.sizeAndPosition = createSizeAndPosition(count, vl.sizeAndPosition.itemSize)
+	vlRender(vl)
+}
+
+const vlRender = (vl: VirtualizedList) => {
+	const { start, stop } = getVisibleRange(vl.sizeAndPosition, vl.height, vl.offset)
+	const fragment = document.createElement("div")
+	for (let index = start; index <= stop; index++) {
+		fragment.appendChild(vl.renderRow(index))
+	}
+	vl.inner.style.height = `${getTotalSize(vl.sizeAndPosition)}px`
+	vl.content.style.top = `${getSizeAndPositionForIndex(vl.sizeAndPosition, start).offset}px`
+	morphdom(vl.content, fragment)
+}
 
 const MISSING_STRING = "(missing)"
 const TABLE_ROW_HEIGHT_PX = 30
@@ -258,7 +602,7 @@ export const createTableFromAos = <RowType extends { [key: string]: any }>({
 	DOWNLOAD_CSV[title] = ""
 
 	const colnames = Object.keys(colSpecInit)
-	DOWNLOAD_CSV[title] += colnames.map(x => `"${x}"`).join(",") + "\n"
+	DOWNLOAD_CSV[title] += colnames.map((x) => `"${x}"`).join(",") + "\n"
 
 	// NOTE(sen) Fill in missing spec entries
 	const colSpec: { [key: string]: TableColSpecFinal<RowType> } = {}
@@ -344,7 +688,7 @@ export const createTableFromAos = <RowType extends { [key: string]: any }>({
 			createTableFilterRow(colSpec, (colname: string, filterVal: any) => {
 				colSpec[colname].filterVal = colSpec[colname].filterValProcess(filterVal)
 				aosFiltered = getAosFiltered()
-				VL.setRowCount(virtualizedList, aosFiltered.length)
+				setRowCount(virtualizedList, aosFiltered.length)
 			})
 		)
 
@@ -373,7 +717,7 @@ export const createTableFromAos = <RowType extends { [key: string]: any }>({
 
 		let aosFiltered = getAosFiltered()
 
-		const virtualizedList = VL.createVirtualizedList(tableBodyContainer, {
+		const virtualizedList = createVirtualizedList(tableBodyContainer, {
 			height: tableBodyHeight,
 			rowCount: aosFiltered.length,
 			renderRow: (rowIndex: number) => {
@@ -398,7 +742,7 @@ export const createTableFromAos = <RowType extends { [key: string]: any }>({
 			if (newTableBodyHeight != tableBodyHeight) {
 				tableBodyHeight = newTableBodyHeight
 				tableBodyContainer.style.maxHeight = newTableBodyHeight + "px"
-				VL.setHeight(virtualizedList, newTableBodyHeight)
+				setHeight(virtualizedList, newTableBodyHeight)
 			}
 		}
 
@@ -420,7 +764,7 @@ export const createTableFromAos = <RowType extends { [key: string]: any }>({
 			forRow?.(rowData)
 		}
 
-        globalThis.window.addEventListener("resize", regenBody)
+		globalThis.window.addEventListener("resize", regenBody)
 	}
 
 	return table
